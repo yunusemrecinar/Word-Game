7eb1c8063e3efe4b4eed4b6615b8f92040c34622
Yunus Emre <www.yunuscinar4141@gmail.com>
2023-12-16 22:36:41 +0300

    contact page text updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className="contact">
             <h1>Yunus Emre Çınar</h1>
-            <p>This is simply a Word Game in which I'm gonna put the words that <br/> I didn't know while I reading the financial books.</p>
+            <p>This is simply a Word Game in which I'm gonna put the words that <br/> I didn't know while I reading the finance books.</p>
         </div>
     )
 };
